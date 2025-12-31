bad1e571ecadc5cc0efff43ba1e5bfcee92d453a
Andrea Tonini <toniniandri23@yahoo.it>
2025-12-31 13:29:14 +0100

    fix: update database name in db.js and correct route handler in profiloRouter.js

--- server/routes/profiloRouter.js
+++ server/routes/profiloRouter.js
@@ -4,6 +4,6 @@ import * as profiloController from '../controllers/profiloController.js';
 
 const router = express.Router();
 
-router.get('/profilo', profiloController.getProfilo);
+router.get('/profilo', profiloController.allPrenotazioniUtente);
 
 export { router as profiloRouter };
